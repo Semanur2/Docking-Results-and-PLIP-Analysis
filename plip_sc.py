33ee5dd79ec2454f84795ee574eb28f6832567ba
Semanur2 <83828249+Semanur2@users.noreply.github.com>
2025-02-27 19:18:13 +0300

    Update plip_sc.py

--- plip_sc.py
+++ plip_sc.py
@@ -1,25 +1,25 @@
 import os
 import subprocess
 
-# Komplekslerin bulunduğu klasör
+# Folder containing the complex structures
 complex_folder = "C:/Users/139sa/Desktop/blac/complexes"
 output_folder = "C:/Users/139sa/Desktop/blac/output_folder"
 
-# Çıktı klasörü yoksa oluştur
+# Create the output folder if it does not exist
 os.makedirs(output_folder, exist_ok=True)
 
-# PLIP'in komut satırı yolunu belirleyin (plip.exe)
+# Define the path to the PLIP executable (plip.exe)
 plip_path = "C:/Users/139sa/miniconda3/envs/myenv36/Scripts/plip.exe"
 
-# Komplekslerin üzerinden geçerek her biri için PLIP analizini çalıştır
+# Iterate through all files in the complex folder and run PLIP analysis for each complex
 for complex_file in os.listdir(complex_folder):
     if complex_file.endswith(".pdb"):
         complex_path = os.path.join(complex_folder, complex_file)
         
-        # Çıktı dosyasının adını kompleks dosyasının adıyla aynı yap
+        # Set the output file name to match the complex file name
         output_file = os.path.join(output_folder, complex_file.replace(".pdb", "_report"))
         
-        # PLIP komutunu çalıştır
+        # Execute the PLIP command
         subprocess.run([plip_path, '-f', complex_path, '-o', output_file, '-t', '-x'])
 
-print("✅ Tüm komplekslerin etkileşim analizleri tamamlandı.")
+print("✅ Interaction analysis for all complexes is complete.")
